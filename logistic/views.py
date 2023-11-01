ad64489e16d436649ea082f2ddfea296f7fd3b57
Garfild13-06 <Garfild13-06@yandex.com>
2023-11-01 22:02:28 +0300

    01.11.2023 22:00

--- logistic/views.py
+++ logistic/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,6 +9,8 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', 'description']
     # при необходимости добавьте параметры фильтрации
 
 
